b43149af4ee43face54dd719ec6560cccd4ace98
Andres <andrest2455@gmail.com>
2021-03-07 17:55:29 -0800

    Wrote fourth test for leap year program to check for years that are not already handled.

--- test_leap_year.py
+++ test_leap_year.py
@@ -11,5 +11,8 @@ class TestLeapYear(unittest.TestCase):
     def test_multiple_of_400(self):
         self.assertEqual(leap_year.leap_year(2000), "Leap year")
 
+    def test_other_year(self):
+        self.assertEqual(leap_year.leap_year(2017), "Not a leap year")
+
 if __name__ == "__main__":
     unittest.main(verbosity=2) 
