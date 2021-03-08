914a874cbbe5818516d88f8c321eb6dc019ac5a8
Andres <andrest2455@gmail.com>
2021-03-07 17:24:11 -0800

    Wrote fourth test for fizzbuzz program to check for numbers that are neither a multiple of 3 or 5.

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -11,5 +11,8 @@ class TestFizzBuzz(unittest.TestCase):
     def test_multiple_of_3_and_5(self):
         self.assertEqual(fizzbuzz.fizzbuzz(60), "FizzBuzz")
 
+    def test_other_number(self):
+        self.assertEqual(fizzbuzz.fizzbuzz(87), "87")
+
 if __name__ == '__main__':
     unittest.main(verbosity=2)
