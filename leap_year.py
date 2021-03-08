68b866d8ed111e0a24d6988efd8c9e98a3bb5b05
Andres <andrest2455@gmail.com>
2021-03-07 17:54:16 -0800

    Implemented code for leap year program to pass third test.

--- leap_year.py
+++ leap_year.py
@@ -1,5 +1,7 @@
 def leap_year(year):
-    if(year % 100 == 0):
+    if(year % 400 == 0):
+        return "Leap year"
+    elif(year % 100 == 0):
         return "Not a leap year"
     elif(year % 4 == 0):
         return "Leap year"
