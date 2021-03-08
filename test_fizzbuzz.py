9a4975af7b6d513ffca8f0cd491854e495726afc
Andres <andrest2455@gmail.com>
2021-03-07 17:19:05 -0800

    Wrote second test for fizzbuzz program to check for multiples of 5.

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -5,5 +5,8 @@ class TestFizzBuzz(unittest.TestCase):
     def test_multiple_of_3(self):
         self.assertEqual(fizzbuzz.fizzbuzz(18), "Fizz")
 
+    def test_multiple_of_5(self):
+        self.assertEqual(fizzbuzz.fizzbuzz(40), "Buzz")
+
 if __name__ == '__main__':
     unittest.main(verbosity=2)
