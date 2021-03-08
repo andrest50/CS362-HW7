c17065f7423c746f99d9bc2b93017f52e3b69704
Andres <andrest2455@gmail.com>
2021-03-07 17:47:42 -0800

    Wrote second test for leap year program to check for multiples of 100.

--- test_leap_year.py
+++ test_leap_year.py
@@ -5,5 +5,8 @@ class TestLeapYear(unittest.TestCase):
     def test_multiple_of_4(self):
         self.assertEqual(leap_year.leap_year(2024), "Leap year")
 
+    def test_multiple_of_100(self):
+        self.assertEqual(leap_year.leap_year(2100), "Not a leap year")
+
 if __name__ == "__main__":
     unittest.main(verbosity=2) 
